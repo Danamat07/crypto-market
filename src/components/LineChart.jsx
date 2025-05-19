84f2e76d1eaba6844dde565baf76f72e800ca917
Matei_Dana <marydanamat@gmail.com>
2025-05-19 16:54:25 +0300

    create crypto details page and chart, fix bug

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Col, Row, Typography } from 'antd';
+
+const { Title } = Typography;
+
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+    if (!coinHistory || !coinHistory.data || !coinHistory.data.history) {
+        return <div>Loading chart data...</div>;
+    }
+
+    const coinPrice = [];
+    const coinTimestamp = [];
+    const history = coinHistory.data.history;
+
+    for (let i = 0; i < history.length; i += 1) {
+        coinPrice.push(history[i].price);
+        coinTimestamp.push(new Date(history[i].timestamp).toLocaleDateString());
+    }
+
+    const data = {
+        labels: coinTimestamp,
+        datasets: [
+            {
+                label: 'Price in USD',
+                data: coinPrice,
+                fill: false,
+                backgroundColor: '#0071bd',
+                borderColor: '#0071bd',
+            },
+        ],
+    };
+
+    const options = {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    };
+
+    return (
+        <>
+            <Row className="chart-header">
+                <Title level={2} className="chart-title">{coinName} Price Chart</Title>
+                <Col className="price-container">
+                    <Title level={5} className="price-change">{coinHistory.data.change}%</Title>
+                    <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
+                </Col>
+            </Row>
+            <Line data={data} options={options} />
+        </>
+    );
+};
+
+export default LineChart;
